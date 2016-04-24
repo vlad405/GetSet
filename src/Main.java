10e7d1edb99ef2e5c4881d002f9e62030232e0d2
vlad405 <vladkotvickiy@mail.ru>
2016-04-24 20:40:27 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import devint.Int;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Int in = new Int();
+        Stroka stri = new Stroka();
+
+        in.setA(10);
+        in.setB(5);
+
+        stri.st = "qwerty";
+
+        System.out.println(in.getA()+" "+in.getB());
+        System.out.println(stri.st);
+
+    }
+
+}
---
+++ src/Stroka.java
@@ -0,0 +1,5 @@
+public class Stroka {
+
+    String st;
+
+}
